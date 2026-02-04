83ef3b7f1b5d9b96d9d787c31337f4eb92bcef8e
Abu <abubakarndu@gmail.com>
2026-02-04 19:23:18 +0300

    fact: update .env.example

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -14,7 +14,7 @@ const logger = createLogger({
   ],
 });
 
-if (process.env.NODE_ENV !== 'production') {
+if (Bun.env.NODE_ENV !== 'production') {
   logger.add(
     new transports.Console({
       format: format.combine(format.colorize(), format.simple()),
